db6219df087a9df6cb0b4ac964bbb6f12ef38417
kesel <a.kiselyoff@gmail.com>
2022-04-03 18:36:52 +0300

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const CHANGE_COLOR_DELAY = 1000;
+let intervalId = null;
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+
+console.log(refs.stopBtn);
+
+refs.stopBtn.setAttribute('disabled', 'disabled');
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  startColorSwitching();
+}
+
+function onStopClick() {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'disabled');
+
+  clearInterval(intervalId);
+}
+
+function startColorSwitching() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  refs.stopBtn.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
